e47c910ce715147959b283ae754e845b18437317
Volkov Bogdan <kkedrovich@gmail.com>
2022-09-23 15:35:44 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def func1():
-    return np.ones(10,10)
+    return np.arange(10)
 
 def func2(x):
-    return np.zeros(10,10)* x
+    return x * np.random.randint(0,100)
 
 def main():
     x = func1()
@@ -15,6 +15,10 @@ def main():
     y = func2(x)
     print(y)
     
-    plt.plot([1,2,3,4], [1,2,3,4], c='g')
+    plt.plot(x, y, c='g')
     plt.show()
+    pass
+
+if __name__ == '__main__':
+    main()
     pass
