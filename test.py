62184099d36e98afa791c170355d5a52e9166a4e
Volkov Bogdan <kkedrovich@gmail.com>
2022-09-26 09:54:02 +0300

    test update

--- test.py
+++ test.py
@@ -2,20 +2,24 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
-
-def func1():
-    return np.arange(10)
+from test_imp import func1
 
 def func2(x):
     return x * np.random.randint(0,100)
 
 def main():
+
     x = func1()
     print(x)
+
     y = func2(x)
     print(y)
     
     plt.plot(x, y, c='g')
+
+    plt.savefig('./figures/foo.png')
+    plt.savefig('./figures/foo.pdf')
+
     plt.show()
     pass
 
---
+++ test_imp.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def func1():
+    return np.arange(10)
+
